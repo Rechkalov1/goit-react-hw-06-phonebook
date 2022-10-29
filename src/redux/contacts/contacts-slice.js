063800bcf5981b8ecd3ed750ae00423cf8f5c562
EnVy <envy7321@gmail.com>
2022-10-29 22:07:47 +0300

    save

---
+++ src/redux/contacts/contacts-slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+const contactsInitialState = { contacts: [] };
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer: (state, { payload }) => {
+        state.push(payload);
+      },
+      prepare: data => {
+        return {
+          payload: {
+            ...data,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+    removeContact: (state, { payload }) => {
+      state.filter(({ id }) => id !== payload);
+    },
+  },
+});
+export const { addContact, removeContact } = contactsSlice.actions;
+export const contactReduser = contactsSlice.reducer;
